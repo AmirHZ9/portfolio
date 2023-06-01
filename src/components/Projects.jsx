a5fc66dce378a33fb3d4474b26e29a2c12684961
unknown <amirzanganeh1999@gmail.com>
2023-06-01 23:09:30 +0330

    design projcet card

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 import { projects } from "../graphql/queries.js";
-
+import "../styles/projects.scss";
 export default function Projects(props) {
   const { loading, data } = useQuery(projects);
 
@@ -11,18 +11,14 @@ export default function Projects(props) {
       ref={props.carRef}
       className="bg-[#f5f5f5] pt-[100px] pb-[130px]"
     >
-      <div
-        className="bold text-center text-3xl md:text-7xl text-header"
-      >
+      <div className="bold text-center text-3xl md:text-7xl text-header">
         PROJECTS
       </div>
 
-      <div
-        className="bg-header w-[70px] h-1 m-auto mt-[25px] mb-[100px]"
-      ></div>
+      <div className="bg-header w-[70px] h-1 m-auto mt-[25px] mb-[100px]"></div>
 
       <div>
-        <ul className="flex justify-center m-auto text-text" >
+        <ul className="flex justify-center m-auto text-text">
           <li className="cursor-pointer mx-6 px-5 md:px-10 text-xs md:text-base font-semibold bg-highlight text-white">
             ALL
           </li>
@@ -38,14 +34,22 @@ export default function Projects(props) {
         {loading
           ? ""
           : data.projects.map((item) => (
-              <div key={item.id}>
-                <a href={item.url}>
-                  <img
-                    src={item.projectCover.url}
-                    alt=""
-                    className="h-[240px] w-[calc(50vw-8px)] sm:h-[300px] md:w-[390px] object-cover"
-                  />
-                </a>
+              <div className="projectsItem" key={item.id}>
+                <img
+                  src={item.projectCover.url}
+                  alt=""
+                  className="h-[240px] w-[calc(50vw-8px)] sm:h-[300px] md:w-[390px] object-cover"
+                />
+
+                <div className="projectItemLink">
+                  <div className="projectInfo">
+                    <p>{item.name}</p>
+                    <span>{item.tech}</span>
+                  </div>
+                  <div className="projectLink">
+                    <a href={item.url}>See More</a>
+                  </div>
+                </div>
               </div>
             ))}
       </div>
